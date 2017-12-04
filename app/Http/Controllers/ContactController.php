d49dd0f9c1c1cd6919b69265d294596adde50ac7
Tim Joosten <Tjoosten3@gmail.com>
2017-12-04 15:46:26 +0100

    Update

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+/**
+ * Class ContactController
+ *
+ * @package App\Http\Controllers
+ */
+class ContactController extends Controller
+{
+    public function store(): View
+    {
+
+    }
+}
